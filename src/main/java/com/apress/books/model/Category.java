ffb69eadc1371583429a0a3ec0088cf327987447
Rekish <rekishg87@gmail.com>
2015-09-11 10:38:32 +0200

    Completed listing01-04

--- src/main/java/com/apress/books/model/Category.java
+++ src/main/java/com/apress/books/model/Category.java
@@ -4,4 +4,27 @@ package com.apress.books.model;
  * Created by Rekish on 9/11/2015.
  */
 public class Category {
+    private Long id;
+    private String categoryDescription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+
+    public String toString() {
+        return "Category - Id: " + id + ", Category Description: "
+                    + categoryDescription;
+    }
 }
